db9ddb671ea0ce89cc3627370f901ec1a025f4ce
Jhonny <jhonnybr391@gmail.com>
2025-10-28 22:48:25 -0300

    a2

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/ObrigadoBabylon/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: '/ObrigadoBabylon/', // 👈 ESSENCIAL
   build: {
-        outDir: 'docs', 
-    },
-  plugins: [react()].filter(Boolean),
+    outDir: 'docs',
+  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
